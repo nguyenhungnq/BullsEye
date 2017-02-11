9b64ffcca9e4682ec06fd4842401574f34b2be3f
nguyenhungnq <NguyenHung@antonio.local>
2017-02-11 11:26:10 +0700

    add assets

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by NguyenHung on 2/10/17.
+//  Copyright © 2017 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+}
